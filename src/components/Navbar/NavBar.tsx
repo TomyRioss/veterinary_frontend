1c9fc6621e559503d810b573d1a0eb215c007a4d
tomy <tomyrios2006@gmail.com>
2024-12-23 19:05:37 -0300

    changes to pnpm and eslint added

---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="flex justify-between items-center w-full h-16 bg-gray-800 text-white"></nav>
+  );
+};
+
+export default NavBar;
